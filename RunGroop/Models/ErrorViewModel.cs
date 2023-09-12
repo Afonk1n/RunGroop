2f694442e72b512c5bf5b8c07a1d2f59620064df
Afonkin Maksim <afona2001@mail.ru>
2023-09-12 23:53:15 +0500

    initial commit

---
+++ RunGroop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RunGroop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
