51179685812f036bb7c53e8fcfbd9825fad1671d
Kirito <afona2001@mail.ru>
2023-09-15 22:27:21 +0500

    add models

---
+++ RunGroop/Data/Enum/ClubCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace RunGroop.Data.Enum
+{
+    public enum ClubCategory
+    {
+        RoadRunner,
+        Womens,
+        City,
+        Trail,
+        Endurance
+    }
+}
---
+++ RunGroop/Data/Enum/RaceCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace RunGroop.Data.Enum
+{
+    public enum RaceCategory
+    {
+        Marathon,
+        Ultra,
+        FiveK,
+        TenK,
+        HalfMarathon
+    }
+}
