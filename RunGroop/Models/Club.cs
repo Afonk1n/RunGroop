ab7703fc4cf3cd9e738594ef42db8ed65b81e3af
Kirito <afona2001@mail.ru>
2023-09-15 23:14:53 +0500

    fix mistake

--- RunGroop/Models/Club.cs
+++ RunGroop/Models/Club.cs
@@ -10,7 +10,7 @@ namespace RunGroop.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string Image { get; set }
+        public string Image { get; set; }
         public int AddressId { get; set; }
         public Address Address { get; set; }
         public ClubCategory ClubCategory { get; set; }
