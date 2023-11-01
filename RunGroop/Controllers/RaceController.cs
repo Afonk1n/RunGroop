38ba37738477655b469e5d64b7658e74bbafd168
Kirito <afona2001@mail.ru>
2023-11-01 22:07:58 +0500

    work on controllers and menu

--- RunGroop/Controllers/RaceController.cs
+++ RunGroop/Controllers/RaceController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunGroop.Data;
+using RunGroop.Models;
 
 namespace RunGroop.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Race> races = _context.Races.ToList();
+            return View(races);
         }
     }
 }
